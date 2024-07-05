383bf6b59e3b21e5698490e2d204d16eaa8979ac
Sanath S Kamath <113462438+SANATHKAMATH18@users.noreply.github.com>
2024-07-05 06:20:32 +0530

    Update portfolio_website.py

--- portfolio_website.py
+++ portfolio_website.py
@@ -4,8 +4,9 @@ import streamlit as st
 from PIL import Image
 import google.generativeai as genai
 
-
-genai.configure(api_key="GOOGLE_API_KEY")
+api_key = st.secrets["GOOGLE_API_KEY"]
+# api_key =""
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 st.markdown(
