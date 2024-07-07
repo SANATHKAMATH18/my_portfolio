aee68e2b455556ec066ce16f24201ae88e2455a5
Sanath S Kamath <113462438+SANATHKAMATH18@users.noreply.github.com>
2024-07-07 06:21:44 +0530

    Update portfolio_website.py

--- portfolio_website.py
+++ portfolio_website.py
@@ -13,11 +13,84 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #e8e8ed66;
-        background-size: cover;
-        background-position: center;
+        background: linear-gradient(135deg, #ece9e6, #ffffff);
+        font-family: 'Arial', sans-serif;
+        color: #333;
+        padding: 20px;
+    }
+    .navbar {
+        display: flex;
+        justify-content: center;
+        background-color: #333;
+        padding: 10px;
+        position: sticky;
+        top: 0;
+        z-index: 1000;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+    .navbar a {
+        color: white;
+        text-decoration: none;
+        padding: 14px 20px;
+        font-size: 18px;
+    }
+    .navbar a:hover {
+        background-color: #575757;
+        border-radius: 4px;
+    }
+    .title {
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 40px;
+        font-size: 36px;
+        color: #444;
+    }
+    .section-title {
+        font-size: 24px;
+        color: #555;
+        margin-bottom: 20px;
+    }
+    .contact {
+        text-align: center;
+        margin-top: 40px;
+    }
+    .contact h2 {
+        font-size: 28px;
+        margin-bottom: 20px;
+    }
+    .contact a {
+        color: #1e90ff;
+        text-decoration: none;
+        font-size: 20px;
+    }
+    .contact a:hover {
+        text-decoration: underline;
+    }
+    .skill-slider .stSlider {
+        background: linear-gradient(90deg, #1e90ff, #00bfff);
+    }
+    .gallery img {
+        border-radius: 8px;
+        margin-bottom: 20px;
+        transition: transform 0.2s;
+    }
+    .gallery img:hover {
+        transform: scale(1.05);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
     }
     </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
+    <div class="navbar">
+        <a href="#">About Me</a>
+        <a href="#skills">Skills</a>
+        <a href="#gallery">Gallery</a>
+        <a href="#contact">Contact</a>
+    </div>
     """,
     unsafe_allow_html=True,
 )
@@ -29,9 +102,8 @@ github_icon = Image.open("icons/github.png")
 
 # Social media URLs
 twitter_url = "https://x.com/Sanath122"
-linkedin_url = "https://www.linkedin.com/in/yourprofile"
-github_url = "https://github.com/yourprofile"
-
+linkedin_url = "https://www.linkedin.com/in/sanath-s-kamath-391b9b24b/"
+github_url = "https://github.com/SANATHKAMATH18"
 
 # Function to convert image to base64
 def image_to_base64(img):
@@ -39,133 +111,111 @@ def image_to_base64(img):
     img.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue()).decode()
 
-
 # Convert images to base64
 twitter_base64 = image_to_base64(twitter_icon)
 linkedin_base64 = image_to_base64(linkedin_icon)
 github_base64 = image_to_base64(github_icon)
 
+st.markdown('<div class="title">Sanath\'s AI Bot</div>', unsafe_allow_html=True)
+
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Hi :wave:")
-    st.title("I am Sanath S Kamath")
+    st.header("I am Sanath S Kamath, a young developing python developer in the field of computer vision.")
 
 with col2:
     st.image("images/sanath.jpg")
-st.title("")
 
+st.markdown('<a name="about"></a>', unsafe_allow_html=True)
 persona = """
-        You are Murtaza AI bot. You help people answer questions about your self (i.e Murtaza)
-        Answer as if you are responding . dont answer in second or third person.
-        If you don't know they answer you simply say "That's a secret"
-        Here is more info about Murtaza: 
+        You are Sanath AI bot. You help people answer questions about yourself (i.e. Sanath)
+        Answer as if you are responding. Don't answer in second or third person.
+        If you don't know the answer, simply say "That's a secret."
+        Here is more info about Sanath: 
 
-        Murtaza Hassan is an Educator/Youtuber/Entrepreneur in the field of Computer Vision and Robotics.
-        He runs one of the largest YouTube channels in the field of Computer Vision,
-        educating over 3 Million developers,
-        hobbyists and students. Murtaza obtained his Bachelor’s degree in
-        Mechatronics and later specialized in the field of Robotics from
-        Bristol University (UK). He is also a serial entrepreneur having launched several
-        successful ventures including CVZone, which is a one stop solution for learning 
-        and building vision projects. Prior to starting his entrepreneurial career, 
-        Murtaza worked as a university lecturer and a design engineer, evaluating 
-        and developing rapid prototypes of US patents.
+        I am Sanath S Kamath from Bangalore, India, 19 years of age born on May 3, 2004. I am currently a student 
+        pursuing my bachelor's in computer science and engineering at Vellore Institute of Technology in Tamil Nadu. 
+        I am interested in Python with computer vision,ai  and development using React and Next.js. I also enjoy playing 
+        sports, especially badminton, cricket, and football. I am quite good at chess. I completed my primary schooling 
+        at Carmel High School in Bangalore and my PU at Base College in Rajajinagar. My mother tongue is Konkani, and I 
+        also speak English, Hindi, and Kannada. My family consists of four members: my father Satish R Kamath, my mother 
+        Reshma S Kamath, and my brother Raksheeth S Kamath. We live as a joint family with my father's brother Suresh R 
+        Kamath, his fiancée Veena S Kamath, and their three children, my cousins Rahul, Megha, and Madhu and along with
+        my grandparents Ramananda kamath and Nirupama kamath. I aspire to become a proficient computer vision developer 
+        in Python. My blood group is B positive. My biggest inspiration is Virat Kohli; I admire him a lot. 
+        my father owns a Hyundai Aura car and two motor vehicles: a TVS scooty and a TVS bike. and my uncle has a Xuv 500
+        I love watching anime, with favorites including One Piece, Bleach, Black Clover, and Hunter x Hunter. I also enjoy 
+        Bollywood and South Indian movies, with favorite actors like Salman Khan, Shah Rukh Khan, and Tiger Shroff. 
+        Ronaldo is my favorite footballer, and Novak Djokovic is my favorite tennis player. My achievements include 
+        obtaining 93% in my 10th boards, 95% in my 12th boards, and several sports medals during my school days.
 
-        Murtaza's Youtube Channel: https://www.youtube.com/channel/UCYUjYU5FveRAscQ8V21w81A
-        Murtaza's Email: contact@murtazahassan.com 
-        Murtaza's Facebook: https://www.facebook.com/murtazasworkshop
-        Murtaza's Instagram: https://www.instagram.com/murtazasworkshop/
-        Murtaza's Linkdin: https://www.linkedin.com/in/murtaza-hassan-8045b38a/
-        Murtaza's Github :https://github.com/murtazahassan
+        Sanath's YouTube Channel: https://www.youtube.com/channel/UCJJRaMQLv_XaAA8mzdP21oA
+        Sanath's Email: sanathkamath11@gmail.com
+        Sanath's Facebook: https://www.facebook.com/sanath.kamath.1/
+        Sanath's Instagram: https://www.instagram.com/_sanath_18/
+        Sanath's LinkedIn: https://www.linkedin.com/in/sanath-s-kamath-391b9b24b/
+        Sanath's GitHub: https://github.com/SANATHKAMATH18
         """
 
-st.title("Sanath's AI Bot")
-
 user_question = st.text_input("Ask anything about me")
-if st.button("ASK", use_container_width=400):
+if st.button("ASK"):
     prompt = persona + "Here is the question that the user asked: " + user_question
     response = model.generate_content(prompt)
     st.write(response.text)
 
-st.title(" ")
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("Youtube channel")
-    st.write("- Largest Computer Vision Channel")
-    st.write("- 400k+ Subscribers")
-    st.write("- Over 150 free tutorials")
-    st.write("- 15 Million+ Views")
-    st.write("- 1.5 Million Hours+ Watch time")
-with col2:
-    st.video("https://youtu.be/BFlRmIvqwSA?si=a6qL3")
+st.markdown('<a name="skills"></a>', unsafe_allow_html=True)
+st.title("")
+st.title("My Skills")
+st.slider("Programming in Python", 0, 100, 60, key="skill_slider")
+st.slider("Teaching", 0, 100, 50, key="skill_slider2")
+st.slider("Web skills", 0, 100, 50, key="skill_slider3")
 
-st.title(" ")
+st.markdown('<a name="gallery"></a>', unsafe_allow_html=True)
+st.title("My projects")
+
+images = [
+    "images/g1.png", "images/g2.jpg", "images/g3.jpg",
+    "images/g4.jpg", "images/g5.jpg", "images/g6.png",
+    "images/g7.png", "images/g8.png", "images/g9.png"
+]
+
+# Desired size for all images
+desired_size = (200, 200)
+
+cols = st.columns(3)
+for i, image_path in enumerate(images):
+    # Open and resize image
+    image = Image.open(image_path)
+    image = image.resize(desired_size)
+
+    # Display image in the appropriate column
+    col = cols[i % 3]
+    col.image(image, use_column_width=True)
+
+
+
 st.title("My Setup")
 st.image("images/setup.jpg")
 
-st.title(" ")
-st.title("My Skills")
-st.slider("Programming", 0, 100, 40)
-st.slider("Teaching", 0, 100, 50)
-st.slider("Robotics", 0, 100, 20)
+st.title("My favorite anime scene")
+st.video("https://youtu.be/thMXb5r792s?si=6UYnoseyuHsWdJnS")
 
-st.title("")
-st.title("Gallery")
 
-#without loops
-# col1, col2, col3 = st.columns(3)
-#
-# with col1:
-#     st.image("images/g1.jpg")
-#     st.image("images/g2.jpg")
-#     st.image("images/g3.jpg")
-# with col2:
-#     st.image("images/g4.jpg")
-#     st.image("images/g5.jpg")
-#     st.image("images/g6.jpg")
-# with col3:
-#     st.image("images/g7.jpg")
-#     st.image("images/g8.jpg")
-#     st.image("images/g9.jpg")
+st.markdown('<a name="contact"></a>', unsafe_allow_html=True)
+st.title("Contact")
+st.write("For any inquiries, please email me at:")
+st.subheader("sanathkamath11@gmail.com")
 
-# with loops
-images = [
-    "images/g1.jpg", "images/g2.jpg", "images/g3.jpg",
-    "images/g4.jpg", "images/g5.jpg", "images/g6.jpg",
-    "images/g7.jpg", "images/g8.jpg", "images/g9.jpg"
-]
-
-# Create three columns
 cols = st.columns(3)
-
-# Loop through the images and assign them to columns
-for i, image in enumerate(images):
-    col = cols[i % 3]
-    col.image(image)
-
-st.subheader("")
-st.write("Contact")
-st.title("For any inquiries,please email me at")
-st.subheader("contact at sanathkamath11@gmail.com")
-
-# Create columns for social media icons
-cols = st.columns(3)
-
-# Display social media icons with clickable links
 with cols[0]:
-    if st.button('Twitter'):
-        st.markdown(
-            f'<a href="{twitter_url}" target="_blank"><img src="data:image/png;base64,{twitter_base64}" width="50"></a>',
-            unsafe_allow_html=True)
-
+    st.markdown(
+        f'<a href="{twitter_url}" target="_blank"><img src="data:image/png;base64,{twitter_base64}" width="50"></a>',
+        unsafe_allow_html=True)
 with cols[1]:
-    if st.button('LinkedIn'):
-        st.markdown(
-            f'<a href="{linkedin_url}" target="_blank"><img src="data:image/png;base64,{linkedin_base64}" width="50"></a>',
-            unsafe_allow_html=True)
-
+    st.markdown(
+        f'<a href="{linkedin_url}" target="_blank"><img src="data:image/png;base64,{linkedin_base64}" width="50"></a>',
+        unsafe_allow_html=True)
 with cols[2]:
-    if st.button('GitHub'):
-        st.markdown(
-            f'<a href="{github_url}" target="_blank"><img src="data:image/png;base64,{github_base64}" width="50"></a>',
-            unsafe_allow_html=True)
+    st.markdown(
+        f'<a href="{github_url}" target="_blank"><img src="data:image/png;base64,{github_base64}" width="50"></a>',
+        unsafe_allow_html=True)
